adcacc7bb581e2d781c4412856a4329e7acabf4e
Dzavialov <dimazua1409@gmail.com>
2022-10-25 01:27:51 +0300

    Extra task

--- HW3Extra/HW3Extra/Program.cs
+++ HW3Extra/HW3Extra/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            DateTime newYear1 = new DateTime(2022, 01, 01);
+            DateTime newYear2 = new DateTime(2023, 01, 01);
+
+            Console.WriteLine($"{Math.Round((DateTime.Now - newYear1).TotalDays)} days passed from New Year");
+            Console.WriteLine($"{Math.Round((newYear2 - DateTime.Now).TotalDays)} days left to New Year");
         }
     }
 }
