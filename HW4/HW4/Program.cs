a234af62b7d8e119ac2d2ddfca3c4315ec3f42f6
Dzavialov <dimazua1409@gmail.com>
2022-10-27 18:39:26 +0300

    Created a project and did the first task

---
+++ HW4/HW4/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace HW4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int X = 10;
+            const int Y = 10;
+            int sum = 0;
+
+            if(X < Y)
+            {
+                for(int i = X; i <= Y; i++)
+                {
+                    sum += i;
+                }
+            } else if (X > Y)
+            {
+                for(int i = Y; i <= X; i++)
+                {
+                    sum += i;
+                }
+            } else
+            {
+                sum = X;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
