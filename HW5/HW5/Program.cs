a9078524bfd8d5458e0fbe708f22ddfec5f2a8b2
Dzavialov <dimazua1409@gmail.com>
2022-10-30 15:56:42 +0200

    Overload MaxValue method with 3 parameters

--- HW5/HW5/Program.cs
+++ HW5/HW5/Program.cs
@@ -7,7 +7,8 @@
             Console.WriteLine(MaxValue(6,6));
             Console.WriteLine(MinValue(2,2));
             int sum;
-            Console.WriteLine(TrySumIfOdd(10, 15, out sum)); 
+            Console.WriteLine(TrySumIfOdd(11, 16, out sum));
+            Console.WriteLine(MaxValue(10,1,2));
         }
 
         static int MaxValue(int value1, int value2)
@@ -47,6 +48,23 @@
                 return false;
             }
             else return true;
+        }
+
+        static int MaxValue(int value1, int value2, int value3)
+        {
+            if (value1 > value2)
+            {
+                if (value1 > value3)
+                {
+                    return value1;
+                }
+                else return value3;
+            }
+            else if (value2 > value3)
+            {
+                return value2;
+            }
+            else return value3;
         }
     }
 }
