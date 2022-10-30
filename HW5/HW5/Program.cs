53f324c7c963f31b9d90f2a724c0eb417f08349f
Dzavialov <dimazua1409@gmail.com>
2022-10-30 15:34:21 +0200

    Method that returns max value of two parameters

--- HW5/HW5/Program.cs
+++ HW5/HW5/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(MaxValue(6,6));
+        }
+
+        static int MaxValue(int value1, int value2)
+        {
+            if(value1 > value2)
+            {
+                return value1;
+            } else if (value1 < value2)
+            {
+                return value1 = value2;
+            } else return value1;
         }
     }
 }
