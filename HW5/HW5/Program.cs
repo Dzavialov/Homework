68a9bd63a13df52d8dc0f5bbe1095d36e58400cb
Dzavialov <dimazua1409@gmail.com>
2022-10-30 16:05:36 +0200

    Overload MinValue method with 4 parameters

--- HW5/HW5/Program.cs
+++ HW5/HW5/Program.cs
@@ -9,6 +9,7 @@
             int sum;
             Console.WriteLine(TrySumIfOdd(11, 16, out sum));
             Console.WriteLine(MaxValue(10,1,2));
+            Console.WriteLine(MinValue(10,20,0,50));
         }
 
         static int MaxValue(int value1, int value2)
@@ -18,7 +19,7 @@
                 return value1;
             } else if (value1 < value2)
             {
-                return value1 = value2;
+                return value2;
             } else return value1;
         }
 
@@ -65,6 +66,28 @@
                 return value2;
             }
             else return value3;
+        }
+
+        static int MinValue(int value1, int value2, int value3, int value4)
+        {
+            int min1;
+            int min2;
+
+            if (value1 < value2)
+            {
+                min1 = value1;
+            }
+            else min1 = value2;
+            if(value3 < value4)
+            {
+                min2 = value3;
+            } else min2 = value4;
+
+            if (min1 < min2)
+            {
+                return min1;
+            }
+            else return min2;
         }
     }
 }
