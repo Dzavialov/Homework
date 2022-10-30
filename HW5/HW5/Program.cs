4e9278bd7e6bfa13e8b89da5f93e8e2f492ecbcd
Dzavialov <dimazua1409@gmail.com>
2022-10-30 15:46:02 +0200

    Return min value + TrySumIfOdd method

--- HW5/HW5/Program.cs
+++ HW5/HW5/Program.cs
@@ -5,6 +5,9 @@
         static void Main(string[] args)
         {
             Console.WriteLine(MaxValue(6,6));
+            Console.WriteLine(MinValue(2,2));
+            int sum;
+            Console.WriteLine(TrySumIfOdd(10, 15, out sum)); 
         }
 
         static int MaxValue(int value1, int value2)
@@ -16,6 +19,34 @@
             {
                 return value1 = value2;
             } else return value1;
+        }
+
+        static int MinValue(int value1, int value2)
+        {
+            if (value1 < value2)
+            {
+                return value1;
+            }
+            else if (value1 > value2)
+            {
+                return value2;
+            }
+            else return value2;
+        }
+
+        static bool TrySumIfOdd(int value1, int value2, out int sum)
+        {
+            sum = 0;
+            for (int i = value1 + 1; i < value2; i++)
+            {
+                sum += i;
+            }
+
+            if (sum % 2 == 0)
+            {
+                return false;
+            }
+            else return true;
         }
     }
 }
