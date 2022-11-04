42de8091892329c9b9ec7977ee75d60360bc3994
Dzavialov <58293091+Dzavialov@users.noreply.github.com>
2022-11-04 14:02:37 +0200

    Add files via upload

---
+++ HW4/HW4/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace HW4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+                Console.WriteLine("Enter X:");
+                string sX = Console.ReadLine();
+                Console.WriteLine("Enter Y:");
+                string sY = Console.ReadLine();
+
+            //const int X = 10;
+            //const int Y = 10;
+            int X = -1;
+            int Y = -1;
+            int sum = 0;
+
+            if (int.TryParse(sX, out X) & int.TryParse(sY, out Y))
+            {
+                if (X < Y)
+                {
+                    for (int i = X; i <= Y; i++)
+                    {
+                        sum += i;
+                    }
+                }
+                else if (X > Y)
+                {
+                    for (int i = Y; i <= X; i++)
+                    {
+                        sum += i;
+                    }
+                }
+                else
+                {
+                    sum = X;
+                }
+                Console.WriteLine(sum);
+            }
+            else
+            {
+                Console.WriteLine("Invalid input");
+            }
+
+            
+        }
+    }
+}
