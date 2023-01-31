c6e2a3544ea0f8720a0fe594be07e9b7888a54cc
Dzavialov <dimazua1409@gmail.com>
2023-01-31 14:22:52 +0200

    Did saync homework

---
+++ Homework/Homework/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json;
+
+namespace Homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many facts you want to see?");
+            int count = int.Parse(Console.ReadLine());
+            HttpClient client = new HttpClient();
+            var responseTask = client.GetAsync("https://meowfacts.herokuapp.com/?count=" + count);
+            responseTask.Wait();
+            if(responseTask.IsCompleted)
+            {
+                var result = responseTask.Result;
+                if(result.IsSuccessStatusCode)
+                {
+                    var fact = JsonSerializer.Deserialize<Fact>(result.Content.ReadAsStreamAsync().Result);
+                    foreach(var x in fact.data)
+                    {
+                        Console.WriteLine("\n" + x);
+                    }
+                }
+            }
+        }
+    }
+}
--- VotingApp/Vote.DataAccess/IRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Vote.DataAccess
-{
-    public interface IRepository<TEntity>
-    {
-        IList<TEntity> GetAll();
-        TEntity Get(int id);
-        void Create(TEntity entity);
-    }
-}
