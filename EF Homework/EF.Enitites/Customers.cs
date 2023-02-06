0709e150a822d1f32ecb2ee46071e8371ea680ca
Dzavialov <dimazua1409@gmail.com>
2023-02-06 12:35:25 +0200

    Declared models

---
+++ EF Homework/EF.Enitites/Customers.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EF.Enitites
+{
+    public class Customers
+    {
+        public int Id { get; set; }
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+        [MaxLength(50)]
+        public string LastName { get; set; }
+        [MaxLength(15)]
+        public int PhoneNumber { get; set; }
+        [MaxLength(256)]
+        public string Email { get; set; }
+    }
+}
---
+++ EF Homework/EF.Enitites/Orders.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF.Enitites
+{
+    public class Orders
+    {
+        public int Id { get; set; }
+        public int TotalAmount { get; set; }
+        public int CustomerId { get; set; }
+        public Customers Customer { get; set; }
+        public int EmployeeId { get; set; }
+        public Employees Employee { get; set; }
+
+    }
+}
