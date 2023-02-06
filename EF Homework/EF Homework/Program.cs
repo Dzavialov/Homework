2d82cafcda83236f2345cc89bedcb8046aca5cba
Dzavialov <dimazua1409@gmail.com>
2023-02-06 13:05:35 +0200

    Uncommented Program.cs

--- EF Homework/EF Homework/Program.cs
+++ EF Homework/EF Homework/Program.cs
@@ -6,40 +6,40 @@ namespace EF_Homework
     {
         static void Main(string[] args)
         {
-            //using(var context = new ShopDbContext())
-            //{
-            //    var customer1 = new Customers()
-            //    {
-            //        FirstName = "Fname1",
-            //        LastName = "Lname1",
-            //        PhoneNumber = 1234561233,
-            //        Email = "email1@em.com"
-            //    };
+            using (var context = new ShopDbContext())
+            {
+                var customer1 = new Customers()
+                {
+                    FirstName = "Fname1",
+                    LastName = "Lname1",
+                    PhoneNumber = 1234561233,
+                    Email = "email1@em.com"
+                };
 
-            //    context.Customers.Add(customer1);
+                context.Customers.Add(customer1);
 
-            //    var customer2 = new Customers()
-            //    {
-            //        FirstName = "Fname2",
-            //        LastName = "Lname2",
-            //        PhoneNumber = 987654321,
-            //        Email = "email2@em.com"
-            //    };
+                var customer2 = new Customers()
+                {
+                    FirstName = "Fname2",
+                    LastName = "Lname2",
+                    PhoneNumber = 987654321,
+                    Email = "email2@em.com"
+                };
 
-            //    context.Customers.Add(customer2);
+                context.Customers.Add(customer2);
 
-            //    var customer3 = new Customers()
-            //    {
-            //        FirstName = "Fname3",
-            //        LastName = "Lname3",
-            //        PhoneNumber = 564738291,
-            //        Email = "email3@em.com"
-            //    };
+                var customer3 = new Customers()
+                {
+                    FirstName = "Fname3",
+                    LastName = "Lname3",
+                    PhoneNumber = 564738291,
+                    Email = "email3@em.com"
+                };
 
-            //    context.Customers.Add(customer3);
+                context.Customers.Add(customer3);
 
-            //    context.SaveChanges();
-            //}
+                context.SaveChanges();
+            }
 
             using (var context = new ShopDbContext())
             {
